79706c0b0c53c13a66b27212d75bc8fa98853fde
Mehdi <Mehdi_Aguendich@protonmail.com>
2022-09-22 15:11:12 +0100

    app deployed

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
 module.exports = {
-    reactStrictMode: true
+    reactStrictMode: true,
+    images: {
+        loader: 'akamai',
+        path: 'https://dovah03.github.io/React-Next-portfolio/out',
+        basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+        assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH
+        
+    },
+      
 }
