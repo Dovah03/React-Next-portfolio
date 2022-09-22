a150c0cbb329e16483b7a6334948b4e90db9c815
Mehdi <Mehdi_Aguendich@protonmail.com>
2022-09-22 17:43:15 +0100

    base path configured

--- next.config.js
+++ next.config.js
@@ -2,9 +2,8 @@ module.exports = {
     reactStrictMode: true,
     images: {
         loader: 'akamai',
-        path: 'https://dovah03.github.io/React-Next-portfolio/',
-        basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-        assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH   
+        path: 'https://dovah03.github.io/React-Next-portfolio/',  
     },
-      
+    basePath: "/React-Next-portfolio",
+    assetPrefix: "/React-Next-portfolio",
 }
