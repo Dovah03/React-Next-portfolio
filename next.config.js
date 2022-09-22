49c4af17d867e7b3a8417468de9e7c5f4ae7ac91
Mehdi <Mehdi_Aguendich@protonmail.com>
2022-09-22 17:11:07 +0100

    prob

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
         loader: 'akamai',
         path: 'https://dovah03.github.io/React-Next-portfolio/',
         basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-        assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH   
+        assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH
     },
       
 }
