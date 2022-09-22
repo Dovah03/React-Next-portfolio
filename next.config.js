e34669f6860b8e6edbfcb7757e300228143ce9bd
Mehdi <Mehdi_Aguendich@protonmail.com>
2022-09-22 14:44:08 +0100

    next conf file upated

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
     reactStrictMode: true,
     images: {
         loader: 'akamai',
-        path: 'https://example.com/',
+        path: 'https://dovah03.github.io/React-Next-portfolio/',
     },
       
 }
