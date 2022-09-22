4388f5a687ea1b0714e4699602a01617e8687339
Mehdi <Mehdi_Aguendich@protonmail.com>
2022-09-22 14:56:15 +0100

    added base path to deploy on github-pages

--- next.config.js
+++ next.config.js
@@ -2,7 +2,9 @@ module.exports = {
     reactStrictMode: true,
     images: {
         loader: 'akamai',
-        path: 'https://dovah03.github.io/React-Next-portfolio/',
+        path: 'https://dovah03.github.io/React-Next-portfolio/out',
+        basePath: 'https://dovah03.github.io/React-Next-portfolio/out',
+        assetPrefix: 'https://dovah03.github.io/React-Next-portfolio/out'
     },
       
 }
