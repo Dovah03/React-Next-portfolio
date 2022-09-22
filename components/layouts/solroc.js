5fdabada655a355cacfe1a67f223a89221370556
Dovah03 <91430461+Dovah03@users.noreply.github.com>
2022-09-22 14:24:49 +0000

    Deploying to gh-pages from @ Dovah03/React-Next-portfolio@804369b2de6daf2dd06032d7d8f4d2b65c39485e 🚀

--- components/layouts/solroc.js
+++
@@ -1,7 +0,0 @@
-import SolrocImage from "../../public/images/solroc.png"
-
-const Solroc = () => (
-    <Image src={SolrocImage} alt="solroc" className="grid-item-thumbnail" placeholder="blur"></Image>
-)
-
-export default Solroc
--- components/no-ssr.js
+++
@@ -1,8 +0,0 @@
-import dynamic from "next/dynamic"
-import React from "react"
-
-const NoSSR = props => <>{props.children}</>
-
-export default dynamic(() => Promise.resolve(NoSSR), {
-    ssr: false
-})
