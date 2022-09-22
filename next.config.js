21bdcf8357720c7d8100e183bc4d2b1f030d4877
Mehdi <Mehdi_Aguendich@protonmail.com>
2022-09-22 18:35:11 +0100

    changes 4

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
     reactStrictMode: true,
     images: {
         loader: 'akamai',
-        path: 'https://dovah03.github.io/',  
+        path: 'https://dovah03.github.io/React-Next-portfolio/',  
     },
     basePath: "/React-Next-portfolio",
     assetPrefix: "/React-Next-portfolio",
