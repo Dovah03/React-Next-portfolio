bd049410e3c5cf99007b8852d1cee63fa3a7da36
Mehdi AGUENDICH <mehdi@audakhla.ma>
2023-02-23 16:27:43 +0100

    commit stage fixed

--- pages/_document.js
+++ pages/_document.js
@@ -1,24 +1,23 @@
-import { ColorModeScript } from "@chakra-ui/react";
-import NextDocument, { Html, Main, NextScript} from 'next/document'
-import Head from 'next/head' 
+import { ColorModeScript } from '@chakra-ui/react'
+import NextDocument, { Html, Main, NextScript, Head } from 'next/document'
 import theme from '../libs/theme'
 
 export default class Document extends NextDocument {
-    render () {
-        return(
-            <Html lang="en">
-                <Head>
-                    <meta    
-                        name="Mehdi Aguendich's Portfolio"
-                        content="Welcome to my portfolio website where I show case teh different project I have worked on throughout the years"
-                    />
-                </Head>
-            <body>
-                <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-                <Main />
-                <NextScript />
-            </body>
-            </Html>
-        )
-    }
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta
+            name="Mehdi Aguendich's Portfolio"
+            content="Welcome to my portfolio website where I show case teh different project I have worked on throughout the years"
+          />
+        </Head>
+        <body>
+          <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
