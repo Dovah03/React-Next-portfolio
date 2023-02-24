39f3dd4be8a47249ddf9c450be4e426acd6ed7df
Mehdi AGUENDICH <mehdi@audakhla.ma>
2023-02-24 10:25:04 +0100

    commited sitemap

--- pages/sitemap.js
+++ pages/sitemap.js
@@ -1,19 +1,23 @@
-// import sitemap from 'sitemap';
+import sitemap from 'sitemap'
 
-// const sitemap = sm.createSitemap({
-//   hostname: 'https://dovah03.github.io/React-Next-portfoli',
-//   cacheTime: 600000, //600 sec (10 min) cache purge period 
-//   urls: [
-//     { url: '/', changefreq: 'daily', priority: 1.0 },
-//     { url: '/portfolio', changefreq: 'monthly', priority: 0.8 },
-//     { url: '/portfolio/portfolioWebsite', changefreq: 'monthly', priority: 0.5 },
-//     { url: '/portfolio/Isoqualtech', changefreq: 'monthly', priority: 0.5 },
-//     { url: '/portfolio/opencvapp', changefreq: 'monthly', priority: 0.5 },
-//     { url: '/portfolio/crudapp', changefreq: 'monthly', priority: 0.5 },
-//     { url: '/portfolio/aiassistant', changefreq: 'monthly', priority: 0.5 },
-//   ],
-// });
+const sitemap = sm.createSitemap({
+  hostname: 'https://dovah03.github.io/React-Next-portfoli',
+  cacheTime: 600000, //600 sec (10 min) cache purge period
+  urls: [
+    { url: '/', changefreq: 'daily', priority: 1.0 },
+    { url: '/portfolio', changefreq: 'monthly', priority: 0.8 },
+    {
+      url: '/portfolio/portfolioWebsite',
+      changefreq: 'monthly',
+      priority: 0.5
+    },
+    { url: '/portfolio/Isoqualtech', changefreq: 'monthly', priority: 0.5 },
+    { url: '/portfolio/opencvapp', changefreq: 'monthly', priority: 0.5 },
+    { url: '/portfolio/crudapp', changefreq: 'monthly', priority: 0.5 },
+    { url: '/portfolio/aiassistant', changefreq: 'monthly', priority: 0.5 }
+  ]
+})
 
-// export function getSitemap() {
-//   return sitemap.toString();
-// }
+export function getSitemap() {
+  return sitemap.toString()
+}
