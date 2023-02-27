54b020d567bce354112769fe4d398f6b8341b5cf
Mehdi AGUENDICH <mehdi@audakhla.ma>
2023-02-27 10:10:57 +0100

    updated content

--- pages/index.js
+++ pages/index.js
@@ -29,17 +29,19 @@ const Page = () => {
   // Function will execute on click of button
   const onButtonClick = () => {
     // using Java Script method to get PDF file
-    fetch('/React-Next-portfolio/images/Mehdi_CV_En.pdf').then(response => {
-      response.blob().then(blob => {
-        // Creating new object of PDF file
-        const fileURL = window.URL.createObjectURL(blob)
-        // Setting various property values
-        let alink = document.createElement('a')
-        alink.href = fileURL
-        alink.download = 'Mehdi-Aguendich_Resume_En.pdf'
-        alink.click()
-      })
-    })
+    fetch('/React-Next-portfolio/images/Mehdi_CV_English.pdf').then(
+      response => {
+        response.blob().then(blob => {
+          // Creating new object of PDF file
+          const fileURL = window.URL.createObjectURL(blob)
+          // Setting various property values
+          let alink = document.createElement('a')
+          alink.href = fileURL
+          alink.download = 'Mehdi-Aguendich_Resume_En.pdf'
+          alink.click()
+        })
+      }
+    )
   }
   return (
     <Transition>
