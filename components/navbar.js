ad9e21dd07a17c9ed1287960348fa6475b92181e
Mehdi AGUENDICH <mehdi@audakhla.ma>
2023-03-06 10:50:43 +0100

    Source code Url changed

--- components/navbar.js
+++ components/navbar.js
@@ -1,67 +1,67 @@
 import Logo from './logo'
 import NextLink from 'next/link'
 import {
-    Container,
-    Box,
-    Link,
-    Stack,
-    Heading,
-    Flex,
-    Menu,
-    MenuItem,
-    MenuList,
-    MenuButton,
-    IconButton,
-    useColorModeValue
+  Container,
+  Box,
+  Link,
+  Stack,
+  Heading,
+  Flex,
+  Menu,
+  MenuItem,
+  MenuList,
+  MenuButton,
+  IconButton,
+  useColorModeValue
 } from '@chakra-ui/react'
 import { IoLogoGithub } from 'react-icons/io5'
 import { HamburgerIcon } from '@chakra-ui/icons'
 import ThemeButton from './theme-button'
 
-const LinkItem = ({ href, path, _target, children, ...props}) => {
-    const active = path === href
-    const inactiveColor = useColorModeValue('grey200','whiteAlpha.900')
-    return (
-        <NextLink href={href} passHref>
-            <Link
-            p={2}
-            bg={active? 'glassTeal' : undefined}
-            color= {active? '#202023' : inactiveColor}
-            _target={_target}
-            {...props}
-            >
-                {children}
-            </Link>
-        </NextLink>
-    )
+const LinkItem = ({ href, path, _target, children, ...props }) => {
+  const active = path === href
+  const inactiveColor = useColorModeValue('grey200', 'whiteAlpha.900')
+  return (
+    <NextLink href={href} passHref>
+      <Link
+        p={2}
+        bg={active ? 'glassTeal' : undefined}
+        color={active ? '#202023' : inactiveColor}
+        _target={_target}
+        {...props}
+      >
+        {children}
+      </Link>
+    </NextLink>
+  )
 }
 
 const Navbar = props => {
-    const {path} = props
-    return (
-        <Box 
-        position = "fixed"
-        as="nav"
-        w="100%"
-        bg={useColorModeValue('#ffffff40','#20202380')}
-        style= {{backdropFilter:'blur(10px)'}}
-        zIndex = {1}
-        {...props}
-        >
-            <Container 
-                display='flex'
-                p={2} 
-                maxW="container.md"
-                wrap="wrap"
-                align="center"
-                justify="space-between"
-            >
-                <Flex align ="center" mr={5}>
-                    <Heading as="h1" size="lg" letterSpacing={'tighter'}>
-                        <Logo />
-                    </Heading>
-                </Flex>
-                <Stack
+  const { path } = props
+  return (
+    <Box
+      position="fixed"
+      as="nav"
+      w="100%"
+      bg={useColorModeValue('#ffffff40', '#20202380')}
+      style={{ backdropFilter: 'blur(10px)' }}
+      zIndex={1}
+      {...props}
+    >
+      <Container
+        display="flex"
+        p={2}
+        maxW="container.md"
+        wrap="wrap"
+        align="center"
+        justify="space-between"
+      >
+        <Flex align="center" mr={5}>
+          <Heading as="h1" size="lg" letterSpacing={'tighter'}>
+            <Logo />
+          </Heading>
+        </Flex>
+        <Stack
           direction={{ base: 'column', md: 'row' }}
           display={{ base: 'none', md: 'flex' }}
           width={{ base: 'full', md: 'auto' }}
@@ -71,7 +71,7 @@ const Navbar = props => {
         >
           <LinkItem
             _target="_blank"
-            href="https://github.com/dovah03"
+            href="https://github.com/Dovah03/React-Next-portfolio"
             path={path}
             align="right"
             display="inline-flex"
@@ -85,12 +85,9 @@ const Navbar = props => {
           <LinkItem href="/portfolio" path={path}>
             Portfolio
           </LinkItem>
-          
-          
         </Stack>
 
         <Box flex={3} align="right" justify="space-between">
-        
           <ThemeButton />
 
           <Box ml={2} display={{ base: 'inline-block', md: 'none' }}>
@@ -108,19 +105,16 @@ const Navbar = props => {
                 <NextLink href="/portfolio" passHref>
                   <MenuItem as={Link}>Portfolio</MenuItem>
                 </NextLink>
-                <MenuItem
-                  as={Link}
-                  href="https://github.com/dovah03"
-                >
+                <MenuItem as={Link} href="https://github.com/dovah03">
                   View Source Code
                 </MenuItem>
-                        </MenuList>
-                    </Menu>
-                </Box>
-            </Box>
-            </Container>
+              </MenuList>
+            </Menu>
+          </Box>
         </Box>
-    )
+      </Container>
+    </Box>
+  )
 }
 
 export default Navbar
