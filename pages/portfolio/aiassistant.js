5c32b2db313a2bd43de407f700eb2a4f2382fd52
Mehdi AGUENDICH <mehdi@audakhla.ma>
2023-03-06 09:45:13 +0100

    added mavis png

--- pages/portfolio/aiassistant.js
+++ pages/portfolio/aiassistant.js
@@ -1,41 +1,43 @@
-import { Badge, Container, Link, List, ListItem } from "@chakra-ui/react";
-import Transition from "../../components/layouts/animatedLayout";
-import { Meta, Title, ProjectImage } from "../title";
-import Paragraph from "../../components/paragraph"
-import { ExternalLinkIcon } from "@chakra-ui/icons";
+import { Badge, Container, Link, List, ListItem } from '@chakra-ui/react'
+import Transition from '../../components/layouts/animatedLayout'
+import { Meta, Title, ProjectImage } from '../title'
+import Paragraph from '../../components/paragraph'
+import { ExternalLinkIcon } from '@chakra-ui/icons'
 
 const project = () => (
-    <Transition title="Isoqualtech">
-        <Container>
-            <Title>
-                Mavis (Artificial Intelligence Assistant) <Badge>2022-</Badge>
-            </Title>
-            <Paragraph>
-                Mavis is a Jarvis like AI Assistant using Google TTS (Text-To-Speech), STT (Speech-To-Text) API, 
-                OpenWeatherMap API and Wikipedia API ... To perform basic tasks like retrieve the weather in a city, display informations from a wiki page, open a folder or 
-                a webpage by only using voice recognition.
-
-            </Paragraph>
-            <List ml={4} my={4}>
-                <ListItem>
-                    <Meta>Souce Code</Meta>
-                    <Link href = "https://github.com/Dovah03/Mavis-Assistant">
-                    https://github.com/Dovah03/Mavis-Assistant <ExternalLinkIcon mx="2px" />
-                    </Link> 
-                </ListItem>
-                <ListItem>
-                    <Meta>Platform</Meta>
-                    <span>Windows/macOS/Linux</span>
-                </ListItem>
-                <ListItem>
-                    <Meta>Stack</Meta>
-                    <span>Python, Google TTS, Google STT, OWM, Wiki API, Pyjokes</span>
-                </ListItem>
-            </List>
-            <ProjectImage src="/images/works/.png" alt="AI-Assistant" />
-            <ProjectImage src="/images/works/.png" alt="AI-Assistant" />
-        </Container>
-    </Transition>
+  <Transition title="Isoqualtech">
+    <Container>
+      <Title>
+        Mavis (Artificial Intelligence Assistant) <Badge>2022-</Badge>
+      </Title>
+      <Paragraph>
+        Mavis is a Jarvis like AI Assistant using Google TTS (Text-To-Speech),
+        STT (Speech-To-Text) API, OpenWeatherMap API and Wikipedia API ... To
+        perform basic tasks like retrieve the weather in a city, display
+        informations from a wiki page, open a folder or a webpage by only using
+        voice recognition.
+      </Paragraph>
+      <List ml={4} my={4}>
+        <ListItem>
+          <Meta>Souce Code</Meta>
+          <Link href="https://github.com/Dovah03/Mavis-Assistant">
+            https://github.com/Dovah03/Mavis-Assistant{' '}
+            <ExternalLinkIcon mx="2px" />
+          </Link>
+        </ListItem>
+        <ListItem>
+          <Meta>Platform</Meta>
+          <span>Windows/macOS/Linux</span>
+        </ListItem>
+        <ListItem>
+          <Meta>Stack</Meta>
+          <span>Python, Google TTS, Google STT, OWM, Wiki API, Pyjokes</span>
+        </ListItem>
+      </List>
+      <ProjectImage src="/images/projects/Mavis.png" alt="AI-Assistant" />
+      <ProjectImage src="/images/works/Mavis_2.png" alt="AI-Assistant" />
+    </Container>
+  </Transition>
 )
 
 export default project
